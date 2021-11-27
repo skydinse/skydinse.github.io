22ae7956b677f7b007191f116a914bad1129a256
DasPrombelBread <82669166+DasPrombelBread@users.noreply.github.com>
2021-11-27 14:13:56 +0100

    Add files via upload

---
+++ source/script/js/main/navigation-btns.js
@@ -0,0 +1,23 @@
+  function discord() {
+    window.open(
+                "https://discord.gg/WbT4jp6", "_blank");
+  }
+  function team() {
+  location.replace("./team.html ")
+  }
+  function contact() {
+    location.replace("./contact.html ")
+  }
+  function home() {
+    location.replace("./home.html")
+  }
+  function legal() {
+    location.replace("./legal.html ")
+  }
+  function rules() {
+    location.replace("./rules.html ")
+  }
+  function linktree() {
+    window.open(
+                "https://linktr.ee/skydinse", "_blank");
+  }
