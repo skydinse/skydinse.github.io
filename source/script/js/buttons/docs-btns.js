2b1f2d581eeb3979343863ce0ede01ace59f7036
DasPrombelBread <82669166+DasPrombelBread@users.noreply.github.com>
2021-12-09 19:59:12 +0100

    Delete source directory

--- source/script/js/buttons/docs-btns.js
+++
@@ -1,9 +0,0 @@
-function rules_docs() {
-  location.replace("./rules.html ")
-}
-function roles_docs() {
-  location.replace("./ingame/roles.html ")
-}
-function games_docs() {
-  location.replace("./ingame/games.html ")
-}
