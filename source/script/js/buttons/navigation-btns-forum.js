47e5dda13e27337a3043ace2627165fe97c98bce
DasPrombelBread <82669166+DasPrombelBread@users.noreply.github.com>
2022-01-17 22:55:51 +0100

    Add files via upload

---
+++ source/script/js/buttons/navigation-btns-forum.js
@@ -0,0 +1,22 @@
+  function discord() {
+    window.open(
+                "https://discord.gg/WbT4jp6", "_blank");
+  }
+  function support() {
+  location.replace("https://skydinse.github.io/help/support.html ")
+  }
+  function faq() {
+    location.replace("https://skydinse.github.io/help/faq/faq.html ")
+  }
+  function contact() {
+    location.replace("https://skydinse.github.io/docs/about/contact.html ")
+  }
+  function home() {
+    location.replace("https://skydinse.github.io/home.html")
+  }
+  function docs() {
+    location.replace("https://skydinse.github.io/docs/docs.html ")
+  }
+  function link() {
+    location.replace("https://skydinse-forum.de ")
+  }
